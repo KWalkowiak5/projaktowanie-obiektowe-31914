2d1287c0a6b26b52c174eb69748d32aa565db367
Kacper Walkowiak <kwalkowiak5@edu.cdv.pl>
2025-10-11 14:26:06 +0200

    lab1

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿const int requireAge = 18;
+const string accesDeniedMessage ="you must be at least 18 years old";
+const string accesAllowedMesage ="Welcome";
 
-Console.WriteLine("Hello, World!");
+int age =18;
+Console.WriteLine("write your age");
+string input =Console.ReadLine();
+bool success = int.TryParse(input,out age);
+do
+{
+    if (!success)
+    {
+        Console.WriteLine("write a number");
+    }
+    else
+    {
+        if (age < requireAge)
+        {
+            Console.WriteLine(accesDeniedMessage);
+        }
+        else if (age >= requireAge)
+        {
+            Console.WriteLine(accesAllowedMesage);
+        }
+    }
+}while(age >= requireAge);
+        
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,13 @@
+﻿class Vehicle
+{
+    public virtual void Start() => Console.WriteLine("Vehicle Start");
+}
+
+class Car : Vehicle
+{
+    public void  Go() => Console.WriteLine("Car Go");
+}
+class Bike : Vehicle
+{
+    public void Ride() => Console.WriteLine("Bike ride");
+}
