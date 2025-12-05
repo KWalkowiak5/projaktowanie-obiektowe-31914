69fa79257db7169217bfbf2b38e970003b17f225
Kacper Walkowiak <kwalkowiak5@edu.cdv.pl>
2025-12-05 12:12:23 +0100

    test pruba 3

--- lab1/lab1/obj/Release/net9.0/lab1.AssemblyInfo.cs
+++ lab1/lab1/obj/Release/net9.0/lab1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1287c0a6b26b52c174eb69748d32aa565db367")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
--- lab2/lab2/obj/Release/net9.0/lab2.AssemblyInfo.cs
+++ lab2/lab2/obj/Release/net9.0/lab2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1287c0a6b26b52c174eb69748d32aa565db367")]
-[assembly: System.Reflection.AssemblyProductAttribute("lab1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
+[assembly: System.Reflection.AssemblyProductAttribute("lab2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
